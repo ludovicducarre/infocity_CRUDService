388f5ab25f1aacb2f5c802d0e46a7e5cb8978ecc
ludovicducarre <ludovic-ducarre@etud.univtln.fr>
2017-11-22 15:11:51 +0100

    opérationnel

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,28 +1,50 @@
-import business.Town;
-import business.User;
+
 import crud.CrudServiceBean;
+import fr.univtln.lducarre365.infoCity.business.Advert;
+import fr.univtln.lducarre365.infoCity.business.Town;
+import fr.univtln.lducarre365.infoCity.business.User;
 
 public class App {
 
 
     public static void main(String[] args) {
-        /*User user = new User.Builder().setId(156).setEmail("user1resu@user.com")
-                .setLastName("user1").setFirstName("resu").setPassword("pass").build();
-*/
 
         Town town = new Town();
         town.setId(125);
         town.setName("Toulon");
+        town.setCountry("PACA");
+        town.setState("France");
 
-       // System.out.println(user.toString());
+
+
+        User user2 = new User(456,"paul","martin","paulmartin@mail.fr","pass");
+
+        //town.addUser(user2);
+        User user3 = new User.Builder().setId(65).setFirstName("test").setLastName("retest").build();
+
+        Advert advert = new Advert.Builder().setId(1).setMessage("Oye! Oye!").setLocation("rue bidon")
+        .setType("sport").build();
+        advert.setTown(town);
+
+        town.addUser(user2);
+
+
+
+
+        // System.out.println(user.toString());
 
         System.out.println(town.toString());
+        System.out.println(user2.toString());
+        System.out.println(user3.toString());
+        System.out.println(advert.toString());
 
 
 
         CrudServiceBean csb = new CrudServiceBean();
             csb.newTransaction();
             csb.create(town);
+            csb.create(user2);
+            csb.create(advert);
             csb.commit();
             csb.close();
 
